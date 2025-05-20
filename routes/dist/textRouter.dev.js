e1ccc36d5e53b511d00a73ce92e189f0b5c2b1c9
Copter <copterpanyphon@gmail.com>
2025-05-21 01:36:47 +0700

    reset files
    reset files

--- routes/dist/textRouter.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var express = require("express");
-
-var textController = require("./../controllers/textController");
-
-var router = express.Router();
-router.route("/").get(textController.showPage);
-module.exports = router;
