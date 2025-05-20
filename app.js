d20be0c3f8a9a1361bfe92c4d67788930ab41369
Copter <copterpanyphon@gmail.com>
2025-05-21 02:24:59 +0700

    Big changes
    add plenty of Pug files and add CSS file

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 
-const AppError = require("./utils/appError");
 const path = require("path");
-const favicon = require("serve-favicon");
-
-const textRouter = require("./routes/textRouter");
-
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+// Set Pug as the view engine
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 // Serving static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Path to your favicon
-app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+// // Path to your favicon
+// app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
-app.use("/", textRouter);
+
+// Define a route
+app.get('/', (req, res) => {
+  res.render('home', { name: 'Copter' });
+});
 
 module.exports = app;
