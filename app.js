9e3b1f1016ea5df3391230349d635826304daed8
Copter <copterpanyphon@gmail.com>
2025-05-21 19:18:57 +0700

    ready for deployment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Set Pug as the view engine
 app.set("view engine", "pug");
-app.set("views", "./views");
+app.set("views", path.join(__dirname, "views"));
 
 // Serving static files
 app.use(express.static(path.join(__dirname, "public")));
