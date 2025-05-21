db547e0cdbe4ddb9d08593c003ec84d32719a934
Copter <copterpanyphon@gmail.com>
2025-05-21 19:35:42 +0700

    add dotenv

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
